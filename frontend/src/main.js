98337fc8a98da6800848649982429b04f6519899
Zara Lundgren <lundzas@gmail.com>
2020-07-05 22:28:35 +0200

    change css so it works

---
+++ frontend/src/main.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import VueApollo from "vue-apollo";
+import VueRouter from "vue-router";
+
+import apolloClient from "./vue-apollo";
+
+import App from "./App.vue";
+
+Vue.use(VueApollo);
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      components: require("./containers/Articles.vue")
+    }
+  ]
+});
+
+new Vue({
+  apolloProvider,
+  router,
+  render: h => h(App)
+}).$mount("#app");
